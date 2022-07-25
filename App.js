c7d735295d55c78979ee4b2dcb16ba10bf2f73b2
ebbbbby <ocynthiaebele@gmail.com>
2022-07-25 09:17:50 +0100

    Movie App Using javascript

---
+++ App.js
@@ -0,0 +1,50 @@
+//TMDB Api
+const API_KEY  = 'api_key=7ed4c24efa1155c4dbcf642d55f06d92';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+ API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+getMovies(API_URL)
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+
+
+function getMovies(url) {
+    fetch(url).then(res=>res.json()).then(data =>{
+        console.log(data.results)
+        showMovies(data.results );
+
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML= '';
+
+    data.forEach(movie=> {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src= "${IMG_URL+poster_path}" alt="${title}">
+        <div class="movie-info">    
+            <h3>${title}</h3>
+            <span class = "${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class='overview'>  
+         ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    });
+}
+
+function getColor(vote) {
+    if(vote >= 8){
+        return 'green'
+}else if(vote >=5 ){
+    return 'orange'
+}else{
+    return 'red'
+}
+    
+}
